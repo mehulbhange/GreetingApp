05393d467530563f6e952cc2a71fc18ab9008aca
mehulbhange <bhangemehul@gmail.com>
2022-10-11 23:07:02 +0530

    ADDED:[Mehul] greeting message with user name

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.entity.Greeting;
+import com.bridgelabz.greetingapp.entity.User;
 import com.bridgelabz.greetingapp.service.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +18,11 @@ public class GreetingController {
     private IGreetingService greetingService;
 
     @GetMapping("/greeting")
-    public String greeting(){
-        return greetingService.getGreetingMessage();
+    public String greeting(@RequestParam String firstName, @RequestParam String lastName){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.getGreetingMessage(user);
     }
 
 }
